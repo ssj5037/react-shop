f604eab29a9e789de45412743e1bcff4c21deafb
sujin shin <shinsj5037@gmail.com>
2024-01-30 18:27:32 +0900

    add svg button component

---
+++ src/components/RSSvgButton.jsx
@@ -0,0 +1,11 @@
+export default function RSSvgButton({ onClick, className, children }) {
+  return (
+    <button
+      onClick={onClick}
+      className={`flex justify-center items-center rounded-full p-1 hover:bg-blue-50 
+        w-16 h-16 text-blue-900 transition-all ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
