bbcc616b29527fbf67350e13290742835a981064
sujin shin <shinsj5037@gmail.com>
2024-01-30 18:26:43 +0900

    add flex extend in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        'main-banner': "url('./assets/images/banner.jpg')",
-      }
+        "main-banner": "url('./assets/images/banner.jpg')",
+      },
+      flex: {
+        2: "2 2 0%",
+        3: "3 3 0%",
+      },
     },
   },
   plugins: [],
-}
-
+};
