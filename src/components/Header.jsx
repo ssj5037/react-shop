6ff7dcc585e0ae66c82d67b6b88b81ca975db677
sujin shin <shinsj5037@gmail.com>
2024-01-30 18:44:06 +0900

    set nav bar, update router for product types

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,20 +6,38 @@ import RSButton from "./RSButton";
 export default function Header() {
   return (
     <header className="p-5 pb-6 border-b mb-10 shadow-sm">
-      <div className="flex justify-between items-center max-w-screen-xl m-auto">
-        <Link
-          to="/"
-          className="flex items-center justify-center gap-2 text-2xl font-semibold text-blue-400"
-        >
-          <FaReact />
-          <div>React Shop</div>
-        </Link>
-        <div className="flex items-center justify-center gap-5">
+      <div className="flex items-center max-w-screen-xl m-auto">
+        {/* logo */}
+        <div className="mr-20">
+          <Link
+            to="/"
+            className="group flex items-center justify-start gap-2 text-2xl font-semibold text-blue-400"
+          >
+            <FaReact className="group-hover:rotate-180 transition-all duration-300" />
+            <div>React Shop</div>
+          </Link>
+        </div>
+        {/* nav */}
+        <nav className="flex-1 text-blue-950 font-bold ">
+          <ul className="flex gap-10">
+            <li className="hover:text-blue-400">
+              <Link to="/products/all">All</Link>
+            </li>
+            <li className="hover:text-blue-400">
+              <Link to="/products/top">Top</Link>
+            </li>
+            <li className="hover:text-blue-400">
+              <Link to="/products/bottom">Bottom</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* user */}
+        <div className="flex flex-1 items-center justify-end gap-5">
           <Link to="/cart">
-            <BsCart2 className="text-2xl text-blue-400" />
+            <BsCart2 className="text-2xl text-blue-400 hover:text-blue-300" />
           </Link>
           <Link to="/new">
-            <BsPencilSquare className="text-xl text-blue-400" />
+            <BsPencilSquare className="text-xl text-blue-400 hover:text-blue-300" />
           </Link>
           <p className="">
             <b>신수진</b>님 안녕하세요
