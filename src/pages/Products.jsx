76d8eae9fb5ff5330452ffd588235bc4d06c62ba
sujin shin <shinsj5037@gmail.com>
2024-01-30 18:54:13 +0900

    set products html,css

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,16 @@
-export default function Products() {
-    return (
-        <div>
-            Products
-        </div>
-    );
-}
+import { useParams } from "react-router-dom";
+import ProductList from "../components/product/ProductList";
 
+export default function Products() {
+  const { type } = useParams();
+  return (
+    <>
+      <div className="uppercase pb-10 border-b text-center text-6xl mb-10 tracking-widest text-blue-500">
+        {type}
+      </div>
+      <section className="flex justify-center items-center py-5 px-5 xl:px-0">
+        <ProductList />
+      </section>
+    </>
+  );
+}
